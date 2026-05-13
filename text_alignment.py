35d1d45c16d9c6ccbdac5e0c108b9f038b2354fe
Shrutika Bhand <shrutikabhand06@gmail.com>
2026-05-13 23:06:17 +0530

    Added text alignment pattern program

---
+++ text_alignment.py
@@ -0,0 +1,31 @@
+# This program prints a text alignment pattern using the character 'H'.
+# It demonstrates the use of loops and string formatting methods
+# such as rjust(), ljust(), and center() for alignment.
+# The pattern consists of a top cone, pillars, middle belt,
+# bottom pillars, and bottom cone structure.
+# The thickness value controls the overall size of the pattern.
+
+
+
+thickness = int(input()) #This must be an odd number
+c = 'H'
+
+#Top Cone
+for i in range(thickness):
+    print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+
+#Top Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+
+#Middle Belt
+for i in range((thickness+1)//2):
+    print((c*thickness*5).center(thickness*6))    
+
+#Bottom Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))    
+
+#Bottom Cone
+for i in range(thickness):
+    print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
